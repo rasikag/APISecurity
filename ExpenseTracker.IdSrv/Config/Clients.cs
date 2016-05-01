4ee9963cd570046221293d2848a3638c503189fa
Rasika Gayan <rasikagayangunarathna@gmail.com>
2016-05-01 23:32:13 +0530

    start to add identity service

---
+++ ExpenseTracker.IdSrv/Config/Clients.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thinktecture.IdentityServer.Core.Models;
+
+namespace ExpenseTracker.IdSrv.Config
+{
+    public class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new[]
+            {
+                new Client
+                {
+                     Enabled = true,
+                     ClientId = "mvc",
+                     ClientName = "ExpenseTracker MVC Client (Hybrid Flow)",
+                     Flow = Flows.Hybrid,
+                     RequireConsent = true,
+
+                     RedirectUris = new List<string>
+                     {ExpenseTrackerConstants.ExpenseTrackerClient }
+
+                }
+            };
+
+        }
+    }
+}
